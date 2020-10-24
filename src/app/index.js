17dce944305d2daaa7491f426d951941d554c625
Jeison Higuita <jeisonhs93@gmail.com>
2020-10-25 00:18:32 +0200

    feat: app working

--- src/app/index.js
+++ src/app/index.js
@@ -2,9 +2,26 @@ import './style.less'
 import EmailsInput from '../lib'
 
 function init() {
-  const inputContainerNode = document.querySelector('#emails-input')
-  const emailsInput = EmailsInput(inputContainerNode)
+  const $inputContainerNode = document.querySelector('#emails-input')
+  const emailsInput = EmailsInput($inputContainerNode)
   console.log(emailsInput)
+
+  const $buttonAdd = document.getElementById('button-add')
+
+  let counter = 1
+  $buttonAdd.addEventListener('click', event => {
+    event.preventDefault()
+    emailsInput.addEmail(`email${counter}@miro.com`)
+    counter++
+  })
+
+  const $buttonCount = document.getElementById('button-count')
+  $buttonCount.addEventListener('click', event => {
+    event.preventDefault()
+    const numberOfValidEmails = emailsInput.countValidEmails()
+
+    alert(`Valid emails: ${numberOfValidEmails} ✅`)
+  })
 }
 
 init()
